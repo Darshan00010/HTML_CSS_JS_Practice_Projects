79f1b7592c495de34c6f51f7fcbb4a442e860b96
Darshan00010 <109279124+Darshan00010@users.noreply.github.com>
2022-09-28 06:55:58 -0700

    day24

---
+++ cl.js
@@ -0,0 +1,10 @@
+let b=setTimeout(() => {
+    prompt("i will become react developer after 1 month")
+}, 5000);
+
+let c=prompt("u want to see promt say y/n")
+if(c=="n"){
+clearTimeout(b);
+}else{
+    
+}
